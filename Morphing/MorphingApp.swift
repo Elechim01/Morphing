4d925038273355c092203cfe4c88f3ee9375c9b4
Elechim01 <68447158+Elechim01@users.noreply.github.com>
2022-09-24 14:43:22 +0200

    Initial Commit

---
+++ Morphing/MorphingApp.swift
@@ -0,0 +1,17 @@
+//
+//  MorphingApp.swift
+//  Morphing
+//
+//  Created by Michele Manniello on 24/09/22.
+//
+
+import SwiftUI
+
+@main
+struct MorphingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
